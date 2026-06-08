5446347ac5d3d1d9fb6d5f8adfef4ec1f20cf5a0
waimea-Jmckinlay <jcmckinlay@waimea.school.nz>
2026-06-08 12:26:51 +1200

    image not working

--- app/__init__.py
+++ app/__init__.py
@@ -20,12 +20,64 @@ app = Flask(__name__)
 # App Routes Handlers
 #===========================================================
 
+
+
+#------------------------------------------------------------------
+#
+#-------------------------------------------------------------
+import os
+import uuid
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = os.path.join('app', 'static', 'uploads')
+
+@app.post("/creatures/new")
+def add_club():
+    # Get the normal text fields from the form
+    name = request.form.get('name', '').strip()
+    name = html.escape(name)
+
+    species = request.form.get(' species','').strip()
+    species= html.escape(name)
+
+    # Get the file selected via the form
+    logo = request.files.get('createures', None)
+    if not logo or logo.filename == '':
+        flash("There was a problem uploading the image", "error")
+        return redirect("/")
+
+    # Sanitise filename and make it unique
+    filename = secure_filename(logo.filename)
+    random_prefix = uuid.uuid4().hex[:12]
+    unique_filename = f"{random_prefix}_{filename}"
+
+    # Get the path of the upload folder
+    filepath = os.path.join(UPLOAD_FOLDER, unique_filename)
+
+    # Save file to disk
+    logo.save(filepath)
+
+    # Add the form data and the upload filename to the DB
+    with connect_db() as db:
+        sql = "INSERT INTO creature (name, speaies, creature_file) VALUES (?, ?, ?)"
+        params = (name, species , unique_filename)
+        db.execute(sql, params)
+
+        flash(f"Creature '{name}' added", "success")
+        return redirect("/")
 #-----------------------------------------------------------
 # Welcome page
 #-----------------------------------------------------------
 @app.get("/")
 def show_welcome():
     return render_template("pages/welcome.jinja")
+
+#-----------------------------------------------------------
+# Welcome page
+#-----------------------------------------------------------
+@app.get("/creature/new")
+def show_creature_form():
+    return redirect("pages/creature_form.jinja")
 
 
 #-----------------------------------------------------------
@@ -35,7 +87,7 @@ def show_welcome():
 def show_all_creatures():
     with connect_db() as db:
         sql = """
-            SELECT id, species, name
+            SELECT id, species, name, image_file
             FROM creatures
         """
         params = ()
